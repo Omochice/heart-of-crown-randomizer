de63b8222ea3757bd3dc10a5140a71ba7f566e85
Omochice <mochice.mls.ntl@gmail.com>
2024-02-28 02:00:34 +0900

    feat: add exporting princess

---
+++ packages/card/src/basic/princess.ts
@@ -0,0 +1,1 @@
+export { default as princesses } from "../../cards/basic/princess.json" with { type: "json" }
---
+++ packages/card/src/far-eastern-border/princess.ts
@@ -0,0 +1,1 @@
+export { default as princesses } from "../../cards/far-eastern-border/princess.json" with { type: "json" }
