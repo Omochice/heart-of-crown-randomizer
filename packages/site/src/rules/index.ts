99526bd038c53966d687c4a06d4c36536c029185
Omochice <mochice.mls.ntl@gmail.com>
2025-05-05 23:22:51 +0900

    chore: delete astro site

--- packages/site/src/rules/index.ts
+++
@@ -1,4 +0,0 @@
-import { rule as zeroWithTwo } from "./zero-with-two.ts";
-import { rule as includeAllCost } from "./include-all-cost.ts";
-
-export const rules = [zeroWithTwo, includeAllCost];
--- packages/site/src/rules/type.ts
+++
@@ -1,6 +0,0 @@
-import type { CommonCard } from "@heart-of-crown-randomizer/card/type";
-
-export type Rule = {
-  description: string;
-  expr: (cards: CommonCard[]) => boolean;
-};
