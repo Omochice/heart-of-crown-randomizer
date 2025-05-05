a0ac0730372bf901a352857b29c6a064198ccc97
Omochice <mochice.mls.ntl@gmail.com>
2025-05-05 22:15:40 +0900

    refactor: use native ts export

--- packages/card/src/basic/rare.ts
+++ packages/card/src/basic/rare.ts
@@ -1,3 +1,29 @@
-export { default as rares } from "../assets/basic/rare.json" with {
-  type: "json",
-};
+import { RareCard } from "../type";
+
+export const rares = [
+  {
+    id: 15,
+    type: "rare",
+    name: "帝都カリクマ",
+    mainType: ["territory", "succession"],
+    cost: 11,
+    succession: 6,
+    coin: 3,
+    link: 1,
+    effect:
+      "クリンナップフェイズの開始時に、あなたの直轄地があるならば、このカードをセットする。",
+    edition: 0,
+  },
+  {
+    id: 16,
+    type: "rare",
+    name: "皇帝の冠",
+    mainType: ["succession"],
+    cost: 14,
+    succession: 14,
+    coin: 0,
+    link: 0,
+    effect: "",
+    edition: 0,
+  },
+] as const satisfies RareCard[];
--- packages/card/test/basic/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, test } from "vitest";
-import { basics, commons, princesses, rares } from "../../src/basic";
-
-describe("basic/index", () => {
-  test("Each card kind should have individual id", () => {
-    const cards = [...princesses, ...commons, ...rares, ...basics];
-    expect(new Set(cards.map((card) => card.id)).size).toBe(cards.length);
-  });
-});
