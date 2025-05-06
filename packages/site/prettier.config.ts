bde74ef65b7519887e0f78f45332c4cb2379696d
Omochice <44566328+Omochice@users.noreply.github.com>
2025-05-06 14:47:59 +0900

    feat: replace to svelte from astro (#733)
    * chore: delete astro site
    
    * feat: replace to svelte
    
    * refactor: add annotation for pass check
    
    * refactor: move input into label
    
    * chore: disable test
    
    * chore: stop to test
    
    * build: add cloudflare adapter
    
    * chore: add prettier
    
    * style: fmt by prettier

---
+++ packages/site/prettier.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'prettier';
+
+const config = {
+	useTabs: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	printWidth: 100,
+	plugins: ['prettier-plugin-svelte'],
+	overrides: [
+		{
+			files: '*.svelte',
+			options: {
+				parser: 'svelte'
+			}
+		}
+	]
+} as const satisfies Config;
+
+export default config;
