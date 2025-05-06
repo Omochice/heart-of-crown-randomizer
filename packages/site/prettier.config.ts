489d5ca0ca450bc6eeb969271a2e3ffcec648352
Omochice <mochice.mls.ntl@gmail.com>
2025-05-06 14:36:38 +0900

    style: fmt by prettier

--- packages/site/prettier.config.ts
+++ packages/site/prettier.config.ts
@@ -1,19 +1,19 @@
-import type { Config } from "prettier"
+import type { Config } from 'prettier';
 
 const config = {
-  "useTabs": true,
-  "singleQuote": true,
-  "trailingComma": "none",
-  "printWidth": 100,
-  "plugins": ["prettier-plugin-svelte"],
-  "overrides": [
-    {
-      "files": "*.svelte",
-      "options": {
-        "parser": "svelte"
-      }
-    }
-  ]
-} as const satisfies Config
+	useTabs: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	printWidth: 100,
+	plugins: ['prettier-plugin-svelte'],
+	overrides: [
+		{
+			files: '*.svelte',
+			options: {
+				parser: 'svelte'
+			}
+		}
+	]
+} as const satisfies Config;
 
-export default config
+export default config;
