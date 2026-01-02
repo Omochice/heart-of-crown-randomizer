70b00c6490f78c31f4ce00c6db625063d3018a05
Omochice <44566328+Omochice@users.noreply.github.com>
2026-01-03 02:09:37 +0900

    test: add a card component story (#1338)
    * test: add a card component story
    
    * refactor: use `fn()` instead

---
+++ packages/site/src/lib/Card.stories.svelte
@@ -0,0 +1,39 @@
+<script
+	module
+	lang="ts"
+>
+	import { defineMeta } from "@storybook/addon-svelte-csf";
+	import { fn } from "@storybook/test";
+	import Card from "./Card.svelte";
+	import type { CommonCard } from "@heart-of-crown-randomizer/card/type";
+	import { Edition } from "@heart-of-crown-randomizer/card/type";
+
+	const sampleCard: CommonCard = {
+		id: 1,
+		type: "common",
+		name: "Sample Card",
+		cost: 3,
+		hasChild: false,
+		mainType: ["action"],
+		link: 1,
+		effect: "Example card effect text",
+		edition: Edition.BASIC,
+	};
+
+	const { Story } = defineMeta({
+		title: "Components/Card",
+		component: Card,
+	});
+</script>
+
+<Story
+	name="Default"
+	args={{
+		card: sampleCard,
+		onSwipeStart: fn(),
+		onSwipeMove: fn(),
+		onSwipeEnd: fn(),
+		onSwipeCancel: fn(),
+		originalIndex: 0,
+	}}
+/>
